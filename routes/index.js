42da1f0fd96e5667ed8e70119800cde499cf6928
damarisfreg <fregosodmar24@gmail.com>
2023-09-08 23:57:38 -0700

    home page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Jammy Tunes' });
 });
 
 router.get('/auth/google', passport.authenticate(
